d6d97e16e9b97e05619b5b4b1a7d8ca71f085a95
nofar-cohen-tanugi <nofarcohen19@gmail.com>
2023-10-19 14:47:51 +0300

    add style for saturday

--- print-template/src/component/saturday/SaturdayStyle.tsx
+++ print-template/src/component/saturday/SaturdayStyle.tsx
@@ -20,27 +20,37 @@ export const SaturdayStyle = () => {
     return inputSettings.find((x) => x.name === 'parasha');
   }, [inputSettings]);
 
-  console.log(settings);
+  const print = () => {};
 
   return (
     <>
-      <div className='w-full h-screen flex flex-column justify-content-between align-items-start'>
-        <h1 className='m-auto my-3'>
-          {t(`${parasha?.name}`)} {data?.['parasha']}
-        </h1>
+      <div
+        className='w-full h-screen flex flex-column justify-content-between align-items-start p-3'
+        style={{
+          backgroundImage: 'url("../public/images/frame.png")',
+          backgroundSize: '100% 100%',
+          backgroundPosition: 'center',
+          backgroundRepeat: 'no-repeat',
+        }}
+      >
+        <div className='w-full flex justify-content-center'>
+          <h1>
+            {t(`${parasha?.name}`)} {data?.['parasha']}
+          </h1>
+        </div>
         {settings?.map((item) => {
           return (
             <div className='flex'>
-              <p className='text-xl font-bold mx-2'>{t(item.name)}</p>
-              <p className='text-xl'>{data?.[item.name]}</p>
+              <p className='text-2xl font-bold mx-2'>{t(item.name)}</p>
+              <p className='text-2xl'>{data?.[item.name]}</p>
             </div>
           );
         })}
         <Button
           icon={PrimeIcons.SAVE}
           label={t('common:print')}
-          className='w-full'
-          onClick={() => window.print()}
+          className='w-10rem align-self-center'
+          onClick={print}
         />
       </div>
     </>
