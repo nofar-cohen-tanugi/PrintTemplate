959b36e3f8f8a4e1720d229b8037c6fa915663f1
nofar-cohen-tanugi <nofarcohen19@gmail.com>
2023-10-19 15:58:36 +0300

    fix print

--- print-template/src/component/saturday/SaturdayStyle.tsx
+++ print-template/src/component/saturday/SaturdayStyle.tsx
@@ -2,11 +2,15 @@ import { Button } from 'primereact/button';
 import { PrimeIcons } from 'primereact/api';
 import { useTranslation } from 'react-i18next';
 import { useSaturdaySettings } from './useSaturdaySettings';
-import { useMemo } from 'react';
+import { useMemo, useRef } from 'react';
+import { useReactToPrint } from 'react-to-print';
+import { ComponentToPrint } from '../print/ComponentToPrint';
 
 export const SaturdayStyle = () => {
   const { t } = useTranslation(['saturday', 'common'], { lng: 'he' });
   const { inputSettings } = useSaturdaySettings();
+
+  const componentRef = useRef(null);
 
   const data = useMemo(() => {
     return JSON.parse(sessionStorage.getItem('saturdayStyle') ?? '');
@@ -20,18 +24,21 @@ export const SaturdayStyle = () => {
     return inputSettings.find((x) => x.name === 'parasha');
   }, [inputSettings]);
 
-  const print = () => {};
+  const handlePrint = useReactToPrint({
+    content: () => componentRef.current,
+  });
 
   return (
     <>
       <div
-        className='w-full h-screen flex flex-column justify-content-between align-items-start p-3'
+        className='w-full h-screen flex flex-column justify-content-between align-items-start p-6'
         style={{
           backgroundImage: 'url("../public/images/frame.png")',
-          backgroundSize: '100% 100%',
+          backgroundSize: '100% 95%',
           backgroundPosition: 'center',
           backgroundRepeat: 'no-repeat',
         }}
+        ref={componentRef}
       >
         <div className='w-full flex justify-content-center'>
           <h1>
@@ -46,12 +53,14 @@ export const SaturdayStyle = () => {
             </div>
           );
         })}
+
         <Button
           icon={PrimeIcons.SAVE}
           label={t('common:print')}
           className='w-10rem align-self-center'
-          onClick={print}
+          onClick={handlePrint}
         />
+        <ComponentToPrint ref={componentRef} />
       </div>
     </>
   );
