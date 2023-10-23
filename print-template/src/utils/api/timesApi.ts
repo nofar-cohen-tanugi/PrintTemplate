5e1f77fcbef66d9470e5404a04ac34d41b5e6cdd
nofar-cohen-tanugi <nofarcohen19@gmail.com>
2023-10-23 15:29:48 +0300

    get coordinates

--- print-template/src/utils/api/timesApi.ts
+++ print-template/src/utils/api/timesApi.ts
@@ -1,7 +1,7 @@
 import { City } from "../../model/saturday/City.model";
 
-export const getTimesByCity = async (city: City) => {
-    const response = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=,${city.code}&appid=${import.meta.env.VITE_API_KEY}`);
+export const getCoordinatesByCity = async (city: City) => {
+    const response = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city.code}&appid=${import.meta.env.VITE_API_KEY}`);
     if (!response.ok) {
         throw new Error('Network response was not ok');
     }
